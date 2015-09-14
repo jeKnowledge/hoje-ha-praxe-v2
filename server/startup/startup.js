167990882dc5b9650a70d697b8e6e6102f1b50f5
Joao Bernardo <jb.amaro@hotmail.com>
2015-09-14 19:07:58 +0100

    master - Fix typo

--- server/startup/startup.js
+++ server/startup/startup.js
@@ -1,12 +1,12 @@
 Meteor.startup(function () {
   if (Meteor.users.find().fetch().length == 0) {
-    Accounts.createUser({email: 'admin@praxeuc.pt', password: 'veteranos456'})
+    Accounts.createUser({ email: 'admin@praxeuc.pt', password: 'veteranos' })
   }
 
   if (Praxe.find().fetch().length == 0) {
     Praxe.insert({
       state: true,
-      notification: 'Notificação oficial do conselho de vetaranos da Universisdade de Coimbra.',
+      notification: 'Notificação oficial do conselho de vetaranos da Universidade de Coimbra.',
       reason: ''
     })
   }
